dfcfa550bbab06e5b01eb058c315c208e95a5717
Thien Ly Buu Huynh <45794429+xiunhoxiu@users.noreply.github.com>
2022-03-07 10:47:13 +0100

    Update multiclipboard.py

--- multiclipboard.py
+++ multiclipboard.py
@@ -2,4 +2,17 @@ import sys  # = "system" to pass cmds like save, test etc. to the terminal
 import clipboard
 import json
 
-print(sys.argv[1:])
+if len(sys.argv) == 2:
+    command = sys.argv[1]
+    print(command)
+    if command == "save":
+        print("save")
+    elif command == "list":
+        print("load")
+    elif command == "list":
+        print("list")
+    else:
+        print("Unknown command")
+else:
+    print("please pass exactly one command.")
+
