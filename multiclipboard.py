f3c1f0b0293142c265fcb68fc68cbf92081ad96c
Thien Ly Buu Huynh <45794429+xiunhoxiu@users.noreply.github.com>
2022-03-07 11:04:26 +0100

    creating JSON file

--- multiclipboard.py
+++ multiclipboard.py
@@ -1,6 +1,15 @@
-import sys  # = "system" to pass cmds like save, test etc. to the terminal 
+import sys  
 import clipboard
 import json
+
+
+def save_items(filepath, data):
+    with open(filepath, "w") as f:
+        json.dump(data, f) 
+       
+save_items("test.json", {"key": "value"})
+
+
 
 if len(sys.argv) == 2:
     command = sys.argv[1]
