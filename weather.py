3969938ce88a32240445f9d3f408ff9449c1a520
Thien Ly Buu Huynh <45794429+xiunhoxiu@users.noreply.github.com>
2022-03-07 14:48:18 +0100

    project #2 - Weather API fetcher

---
+++ weather.py
@@ -0,0 +1,20 @@
+import requests
+
+
+API_KEY = input("Enter a weather api key: ")
+BASE_URL="http://api.openweathermap.org/data/2.5/weather"
+
+city = input("Enter a city name: ")
+request_url = f"{BASE_URL}?appid={API_KEY}&q={city}"
+response = requests.get(request_url)
+
+if response.status_code == 200:
+    data = response.json()
+    weather = data['weather'][0]['description']
+    print(weather)
+    temperature = round(data["main"]["temp"] - 273.15,2)
+    print("Weather:", weather)
+    print("Temperature:", temperature, "in celsius")
+
+else:
+    print("An error occurred.")
